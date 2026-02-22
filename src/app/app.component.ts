6b6def31726ed442a5e7658cb42a8cdeaeb1b6cf
kunj.shah <kunj.shah>
2026-02-22 18:38:07 +0530

    Infographic

---
+++ src/app/app.component.ts
@@ -0,0 +1,35 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  topics: string[] = ['Business', 'Sport', 'Entertainment', 'Health', 'Productivity'];
+  selectedTopic: string = '';
+
+  // Notice the change here: string[] instead of string
+  infographicData: { [key: string]: string[] } = {
+    'Business': [
+      'assets/biz-1.webp', 
+      'assets/biz-2.webp'
+    ],
+    'Sport': [
+      'assets/sport-1.webp'
+    ],
+    'Entertainment': [
+      'assets/entertainment-1.webp', 
+    ],
+    'Health': [
+      'assets/health-1.webp',
+    ],
+    'Productivity': [
+      'assets/prod-1.webp',
+    ]
+  };
+
+  selectTopic(topic: string): void {
+    this.selectedTopic = topic;
+  }
+}
